1ec6b3abdf99f1cddd5e45cf988599fea366535d
박설희 <pshee.dev@gmail.com>
2025-11-14 14:54:28 +0900

    fix: accounts url 경로 오타 수정

--- mypjt/urls.py
+++ mypjt/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', include('books.urls')),
-    path('accouts/', include('accouts.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
